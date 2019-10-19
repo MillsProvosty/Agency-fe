c594dc603255f52aa5355fb9dc40a195f9d2a06e
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-10-19 15:02:37 -0600

    Edit styles for app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { FaHandsHelping } from "react-icons/fa";
 
 function App() {
   return (
     <section className="App">
-      <FaHandsHelping />
-      <p>Agency is pretty frickin great</p>
+    <FaHandsHelping size={64}/>
+      <h1>Agency</h1>
+      <p>is pretty frickin great</p>
       <section>
-        <button>Volunteer</button>
-        <button>Client</button>
+        <button className="volunteer">Volunteer</button>
+        <button className="client">Client</button>
       </section>
     </section>
   );
