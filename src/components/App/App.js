67f0886d56732a50369e761f781d27e3a88841a2
Aidan McKay <aidanmckay2000@gmail.com>
2019-10-19 14:33:52 -0600

    Conditionally render form with user type

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { FaHandsHelping } from "react-icons/fa";
+import { SignInForm } from '../SignInForm/SignInForm'
 
-function App() {
-  return (
-    <section className="App">
-      <FaHandsHelping />
-      <p>Agency is pretty frickin great</p>
-      <section>
-        <button>Volunteer</button>
-        <button>Client</button>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hideLanding: false,
+      userType: ''
+    }
+  }
+
+  displayForms = (userType) => {
+    this.setState({hideLanding: !this.state.hideLanding, userType})
+  }
+
+  render() {
+    let forms = null
+    if(this.state.hideLanding) {
+      forms = (
+        <SignInForm props={this.state.userType} /> 
+      )
+    }
+    return (
+      <section className="App">
+      {!forms && 
+      <>
+        <FaHandsHelping />
+        <p>Agency is pretty frickin great</p>
+        <section>
+          <button onClick={() => this.displayForms('volunteer')}>Volunteer</button>
+          <button onClick={() => this.displayForms('client')}>Client</button>
+        </section>
+      </>
+      }
+      {forms}
       </section>
-    </section>
-  );
+    );
+  }
 }
 
 export default App;
