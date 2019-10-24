a11aa99125978f7057525ddf8693e2f86acf5908
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-10-24 10:43:44 -0600

    Began password validation

--- src/components/SignUp/SignUpModal.js
+++ src/components/SignUp/SignUpModal.js
@@ -1,51 +1,82 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./SignUpModal.scss";
 import { Link } from "react-router-dom";
 
-export class SignUpModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+export const SignUpModal = () => {
+  const [inputValue, handleChangesInState] = useState({
+    name: "",
+    phone: null,
+    email: "",
+    password: "",
+    confirmation: ""
+  });
 
-  render() {
-    return (
-      <form className="SignUpModal">
-        <p>Thanks for Signing Up!</p>
-        <input
-          type="text"
-          placeholder="Enter your name"
-          name="name"
-          value={this.state.name}
-        />
-        <input
-          type="text"
-          placeholder="Enter your email"
-          name="email"
-          value={this.state.email}
-        />
-        <input
-          type="text"
-          placeholder="Enter your phone"
-          name="phone"
-          value={this.state.phone}
-        />
-        <input
-          type="text"
-          placeholder="Enter your password"
-          name="password"
-          value={this.state.password}
-        />
-        <input
-          type="text"
-          placeholder="Confirm your password"
-          name="confirmation"
-          value={this.state.confirmation}
-        />
-        <Link to="/profile">
-          <button>Submit!</button>
-        </Link>
-      </form>
-    );
-  }
-}
+  const [errorValue, showError] = useState("");
+
+  const handleClick = e => {
+    e.preventDefault();
+ 
+  };
+
+  const handleChange = e => {
+    e.preventDefault();
+    handleChangesInState({ ...inputValue, [e.target.name]: e.target.value });
+    doTheyMatch();
+  };
+
+  const doTheyMatch = () => {
+    if (inputValue.password === inputValue.confirmation) {
+      showError("");
+    } else {
+      showError("Passwords Do Not Match");
+    }
+  };
+
+  
+  return (
+    <form className="SignUpModal">
+      <p>Thanks for Signing Up!</p>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        name="name"
+        value={inputValue.name}
+        onChange={e => handleChange(e)}
+      />
+      <input
+        type="text"
+        placeholder="Enter your email"
+        name="email"
+        value={inputValue.email}
+        onChange={e => handleChange(e)}
+      />
+      <input
+        type="text"
+        placeholder="Enter your phone"
+        name="phone"
+        value={inputValue.phone}
+        onChange={e => handleChange(e)}
+      />
+      <input
+        type="text"
+        placeholder="Enter your password"
+        name="password"
+        value={inputValue.password}
+        onChange={e => handleChange(e)}
+      />
+      <input
+        type="text"
+        placeholder="Confirm your password"
+        name="confirmation"
+        value={inputValue.confirmation}
+        onChange={e => handleChange(e)}
+      />
+      {errorValue && <p>{errorValue}</p>}
+      <Link to="/profile">
+        <button onClick={e => handleClick(e)} disabled={!errorValue}>
+          Submit!
+        </button>
+      </Link>
+    </form>
+  );
+};
