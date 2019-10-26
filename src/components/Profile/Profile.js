dc2d0ef99e0543fcff03e5271bb55ff2eb8fd5d4
Aidan McKay <aidanmckay2000@gmail.com>
2019-10-26 15:55:39 -0600

    display user name on profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./Profile.scss";
 import Nav from "../Nav/Nav";
+import { connect } from 'react-redux';
 
-const Profile = () => {
+const Profile = (props) => {
   return (
     <section className="Profile">
         <Nav />
       <section className="container-dash">
-        <p>ima Profile</p>
+        <p>Hello {props.user.first_name}</p>
       </section>
     </section>
   );
 };
 
-export default Profile;
+export const mapStateToProps = state => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(Profile);
