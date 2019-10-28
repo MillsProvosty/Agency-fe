b7ab7e61db010b62bc7aa90466e68f260b12ac2e
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-10-27 18:34:14 -0600

    Style landing page and sign-in

--- src/components/SignInForm/SignInForm.js
+++ src/components/SignInForm/SignInForm.js
@@ -8,18 +8,23 @@ import Modal from "react-modal";
 import styled from "styled-components";
 import { getSpecificUser } from "../../util/apiCalls";
 import { useSignInForm } from "../../hooks/useForm";
-import {validate} from "../../hooks/signInFormValidationRules";
+import { validate } from "../../hooks/signInFormValidationRules";
 import { setUser } from "../../actions";
 import { connect } from "react-redux";
+import floatingImg from "./floating.svg";
 
 const SignIn = styled.section`
-  height: auto;
-  background-color: white;
   justify-content: center;
+  height: 100vh;
+  width: 100%;
+  background-color: aliceblue;
+  background-repeat: no-repeat;
+  background-position: left top;
+  background-size: cover;
+  background-image: url(${floatingImg});
 `;
 
 const ModalStyle = styled.section`
-  margin-top: 50px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -44,12 +49,13 @@ const Titles = styled.section`
 `;
 
 const SignsSection = styled.section`
-  width: 50%;
+  width: 40%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 20vh;
-  border-right: ${props => (props.sign ? "1px solid darkblue" : "none")};
+  height: 40vh;
+  background-color: white;
+  border: 1px solid #37474E
   @media screen and (max-width: 375px) {
     border-right: none;
   }
@@ -80,14 +86,16 @@ const Headers = styled.h1`
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+  align-items: center;
   justify-content: space-around;
+  margin: auto;
   @media screen and (max-width: 375px) {
-    border-bottom: 2px solid darkblue;
+    border-bottom: 2px solid #37474e;
     padding-bottom: 30px;
     margin-top: 20px;
   }
   @media screen and (display-mode: standalone) {
-    border-bottom: 2px solid darkblue;
+    border-bottom: 2px solid #37474e;
     padding-bottom: 30px;
     margin-top: 20px;
   }
@@ -106,7 +114,7 @@ const Input = styled.input`
 
 const Button = styled.button`
   color: white;
-  background-color: darkblue;
+  background-color: #37474e;
   border-radius: 5px;
   font-size: 2em;
   width: 175px;
@@ -114,8 +122,8 @@ const Button = styled.button`
   border: 2px solid white;
 
   :hover {
-    border: 2px solid darkblue;
-    color: darkblue;
+    border: 2px solid #37474e;
+    color: #37474e;
     background-color: white;
   }
 
@@ -157,15 +165,12 @@ export const SignInForm = props => {
           <SignUpModal />
         </Modal>
       </ModalStyle>
-      <FaHandsHelping size={64} />
+      <FaHandsHelping className="hands-form" size={64} />
       <Container>
-        <Titles>
-          <Headers>Sign In</Headers>
-          <Headers SignUp>Sign Up</Headers>
-        </Titles>
         <Titles second>
           <SignsSection sign>
             <Form>
+              <Headers>Sign In</Headers>
               <Input
                 autoComplete="off"
                 tabIndex={0}
@@ -200,6 +205,7 @@ export const SignInForm = props => {
             </Form>
           </SignsSection>
           <SignsSection>
+            <Headers SignUp>Sign Up</Headers>
             <Button onClick={() => showModal(true)}>Sign Up</Button>
           </SignsSection>
         </Titles>
