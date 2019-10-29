55d00a7962600b718847c7e7f0218864a22d1a27
Gregory Anderson <anderson.gregory.mail@gmail.com>
2019-10-29 08:56:04 -0600

    Delete Profile on line

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -4,19 +4,18 @@ import Nav from "../../components/Nav/Nav";
 import { connect } from "react-redux";
 import { getAllOpportunities, deleteAUser } from "../../util/apiCalls";
 import { setUserOpportunities } from "../../actions";
-import Opportunities from '../Opportunities/Opportunities'
+import Opportunities from "../Opportunities/Opportunities";
 import styled from "styled-components";
 
-
 const ProfileSection = styled.section`
-width: 100%;
-height: 100%;
-background-color: aliceblue;
-background-attachment: fixed;
-@media screen and (max-width: 375px) {
-  justify-content: space-around;
-}
-`
+  width: 100%;
+  height: 100%;
+  background-color: aliceblue;
+  background-attachment: fixed;
+  @media screen and (max-width: 375px) {
+    justify-content: space-around;
+  }
+`;
 
 const Header = styled.h1`
 color: #37474E
@@ -30,33 +29,31 @@ margin: 0px;
 const Profile = props => {
   const [isLoading, setLoading] = useState(true);
 
-
   const getUserOpp = async () => {
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    getUserOpp()
-  }, [])
+    getUserOpp();
+  }, []);
 
   return (
     <ProfileSection className="Profile">
       <Nav />
-    {isLoading &&
-       <p>I am loading</p>
-    }
-    {!isLoading && user.role === 'client' &&
-      <>
-        <Header>Welcome, {props.user.firstname}</Header>
-        <Opportunities role={props.user.role}/>
-      </>
-    }
-    {!isLoading && user.role === 'volunteer' &&
-      <>
-        <Header>Welcome, {props.user.firstname}</Header>
-        <Opportunities role={props.user.role}/>
-      </>
-    }
+      <button onClick={() => deleteAUser(props.user.id)}>Delete Account</button>
+      {isLoading && <p>I am loading</p>}
+      {!isLoading && props.user.role === "client" && (
+        <>
+          <Header>Welcome, {props.user.firstname}</Header>
+          <Opportunities role={props.user.role} />
+        </>
+      )}
+      {!isLoading && props.user.role === "volunteer" && (
+        <>
+          <h1>Welcome {props.user.firstname}</h1>
+          <Opportunities role={props.user.role} />
+        </>
+      )}
     </ProfileSection>
   );
 };
@@ -67,7 +64,11 @@ export const mapStateToProps = state => ({
 });
 
 export const mapDispatchToProps = dispatch => ({
-  setOpportunities: opportunities => dispatch(setUserOpportunities(opportunities))
-})
+  setOpportunities: opportunities =>
+    dispatch(setUserOpportunities(opportunities))
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Profile);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Profile);
